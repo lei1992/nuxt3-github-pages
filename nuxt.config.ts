a50bfb9d1e1ec7eec855dc2457e3f048dafc20f8
lxf <2910986411@qq.com>
2024-04-15 16:14:32 +0800

    feat: actions one

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const path = require('path');
+// const path = require('path');
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  nitro: {
-    output: {
-      publicDir: path.join(__dirname, 'docs')
-    }
-  }
+  // nitro: {
+  //   output: {
+  //     publicDir: path.join(__dirname, 'docs')
+  //   }
+  // }
+  app: {
+    baseURL: '/nuxt3-github-pages/',
+    buildAssetsDir: 'nuxt_assets',
+  },
+  experimental: {
+    payloadExtraction: false
+  },
 })
