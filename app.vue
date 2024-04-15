2b76fb23db1990ff7c20bdbb04d5853aa13d21b1
lxf <2910986411@qq.com>
2024-04-15 17:55:24 +0800

    fix: 删除多余

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    通过 Github Action 部署Nuxt3项目到 Github Pages.
+    通过 Github Action 部署Nuxt3项目到 Github
+    Pages.这样可以通过github链接直接访问项目demo
     <NuxtWelcome />
   </div>
 </template>
