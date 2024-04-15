715642d862b0f5ac49d2c7f80496adb77d8c39da
lxf <2910986411@qq.com>
2024-04-15 17:01:10 +0800

    feat: 使用github actions 部署网页

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// const path = require('path');
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  // nitro: {
-  //   output: {
-  //     publicDir: path.join(__dirname, 'docs')
-  //   }
-  // }
   app: {
     baseURL: '/nuxt3-github-pages/',
-    buildAssetsDir: 'nuxt_assets',
-  },
-  experimental: {
-    payloadExtraction: false
   },
 })
